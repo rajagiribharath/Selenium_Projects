1a8279d8e87423a843d13d3f321c833fd7021e0f
unknown <bharathrajagiri1991@gmail.com>
2018-04-24 12:39:05 +0530

    just added comments

--- src/main/java/com/bhanu/testBase/TestClass.java
+++ src/main/java/com/bhanu/testBase/TestClass.java
@@ -4,6 +4,38 @@ public class TestClass
 {
 public static void main(String[]args)
 {
-	
+	/*git --version
+	 * 
+	 * git clone url of git
+	 * 
+	 * go to path of project
+	 * 
+	 * import into eclipse via  import->maven->existing project-> import
+	 * 
+	 * then go to class
+	 * 
+	 * create new class
+	 * 
+	 * now go to cmd give command 
+	 * 
+	 * git status
+	 * 
+	 * git add .  --> it load all files, if you want to give only one file
+	 * 
+	 * git add filename
+	 * 
+	 * git commit -m  "give the proper comment what you changed"
+	 * 
+	 * git push  --> it will ask login give that and give
+	 * 
+	 * git status
+	 * 
+	 * check git hub by reload
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
 }
 }
